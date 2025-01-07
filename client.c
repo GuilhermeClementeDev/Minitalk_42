6dafda33b076578956f94c89c7e9e8e40c1c94d8
guizindev <guilhermevasco2015@gmail.com>
2025-01-06 21:03:13 -0300

    start working on client

--- client.c
+++ client.c
@@ -1,6 +1,16 @@
 #include "./lib/libft.h"
+#include <signal.h>
 
-int main()
+int main(int argc, char **argv)
 {
+	int pid;
 
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+	}
+	else
+	{
+		ft_printf("Invalid format! \nExpected: ./client <PID> phrase \n");
+	}
 }
